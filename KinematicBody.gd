267d4ded7facddd0ebf00a0155e3de89c53d4903
zhienjbigs <70808102+zhienjbigs@users.noreply.github.com>
2020-11-29 22:31:26 +0800

    Add files via upload

---
+++ KinematicBody.gd
@@ -0,0 +1,36 @@
+extends KinematicBody
+
+var gravity = -9.8
+var velocity = Vector3(0,0,0)
+var camera
+
+const SPEED = 20
+const ACCELERATION = 9
+const DE_ACCELERATION = 7
+
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+
+	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_left"):
+		velocity.x = 0
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x = SPEED
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -SPEED
+	else:
+		velocity.x = lerp(velocity.x,0,0.1)
+
+	if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_down"):
+		velocity.z = 0
+	elif Input.is_action_pressed("ui_up"):
+		velocity.z = -SPEED
+	elif Input.is_action_pressed("ui_down"):
+		velocity.z = SPEED
+	else:
+		velocity.z = lerp(velocity.z,0,0.1)
+	move_and_slide(velocity)
+
+
