16257a53d94cc678892ffe8d80da4e0cb8b85c80
Alexyz-Sylver <2020sha01236@iacademy.edu.ph>
2020-12-13 12:06:13 +0800

    replaced item files

--- Items.gd
+++
@@ -1,4 +0,0 @@
-extends Node2D
-
-func _ready():
-	$Absolutely.texture = load("res://Items/Absolutely.png")
--- Slot.gd
+++
@@ -1,8 +0,0 @@
-extends Panel
-
-var ItemClass = preload("res://Items.tscn")
-var item = null
-
-func _ready():
-	item = ItemClass.instance()
-	add_child(item)
